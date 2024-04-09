e1773ce7be8e0b2d9165391c2f32342720c52f0d
Suren <146822024+DinamoRoller@users.noreply.github.com>
2024-04-09 23:00:31 +0200

    Apartado 6 (archivo complementario al principal)

---
+++ Contacto.java
@@ -0,0 +1,36 @@
+public class Contacto {
+    private String name;
+    private String email;
+    private String phoneNumber;
+
+    public Contacto(String name, String email, String phoneNumber) {
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    // Getters y setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
